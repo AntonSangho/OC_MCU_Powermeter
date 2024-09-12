804c20ae08fea1b93d706d1b9b54d2e613755b09
AntonSangho <sanghoemail@gmail.com>
2024-09-12 13:16:26 +0900

    네오픽셀과 같은 불필요한 코드 제거, 순서도 추가

--- src/powermeter_oled/powermeter_oled.ino
+++ src/powermeter_oled/powermeter_oled.ino
@@ -1,15 +1,5 @@
-// Feather Power Meter
-//
-// Small Feather-based power monitor using an INA219 breakout and
-// monochrome OLED display.
-//
-// Author: Tony DiCola (modded by ladyada)
-//
-// Released under a MIT license: http://opensource.org/licenses/MIT
-
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_NeoPixel.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_INA219.h>
@@ -18,12 +8,9 @@
 // 0 = none, 1 = 90 degrees clockwise, 2 = 180 degrees, 3 = 270 degrees CW
 #define ROTATION     0
 
-#define NEO_PIN 6
-
 // Create NeoPixel, OLED and INA219 globals.
 Adafruit_SSD1306 display;
 Adafruit_INA219 ina219;
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(144, NEO_PIN, NEO_GRB + NEO_KHZ800);
 
 // Keep track of total time and milliamp measurements for milliamp-hour computation.
 uint32_t total_sec = 0;
@@ -31,7 +18,6 @@ float total_mA = 0.0;
 
 uint8_t counter = 0;
 void pixel_show_and_powerupdate() {
-  pixels.show();
   if (counter == 0) {
     update_power_display();
   } else {
@@ -45,8 +31,8 @@ void setup()   {
       // will pause Zero, Leonardo, etc until serial console opens
       delay(1);
   }
-  pixels.begin();
-  pixels.show();  // Initialize all pixels to 'off'
+  // pixels.begin();
+  // pixels.show();  // Initialize all pixels to 'off'
   // Try to initialize the INA219
   if (! ina219.begin()) {
     Serial.println("Failed to find INA219 chip");
@@ -76,107 +62,8 @@ void setup()   {
 }
 
 void loop() {
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(pixels.Color(255, 0, 0), 50); // Red
-  colorWipe(pixels.Color(0, 255, 0), 50); // Green
-  colorWipe(pixels.Color(0, 0, 255), 50); // Blue
-//colorWipe(pixels.Color(0, 0, 0, 255), 50); // White RGBW
-  // Send a theater pixel chase in...
-  theaterChase(pixels.Color(127, 127, 127), 50); // White
-  theaterChase(pixels.Color(127, 0, 0), 50); // Red
-  theaterChase(pixels.Color(0, 0, 127), 50); // Blue
-
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
+  pixel_show_and_powerupdate();
 }
-
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<pixels.numPixels(); i++) {
-    pixels.setPixelColor(i, c);
-    pixel_show_and_powerupdate();
-    delay(wait);
-  }
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    pixel_show_and_powerupdate();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
-    }
-    pixel_show_and_powerupdate();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < pixels.numPixels(); i=i+3) {
-        pixels.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      pixel_show_and_powerupdate();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < pixels.numPixels(); i=i+3) {
-        pixels.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < pixels.numPixels(); i=i+3) {
-        pixels.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      pixel_show_and_powerupdate();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < pixels.numPixels(); i=i+3) {
-        pixels.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
 
 void update_power_display() {
   // Read voltage and current from INA219.
